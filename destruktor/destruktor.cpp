2a883b25d8015d0bafb075d0383a93b163f65e2f
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-17 18:17:02 +0700

    mengisi variabel angka

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -18,3 +18,10 @@ angka::angka(int i) { //Constructor
 	isiData();
 }
 
+angka::~angka() { // Destructor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+

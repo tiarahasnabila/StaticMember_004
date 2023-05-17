14c4058a3263c923335edf9e42cbe9bc61b025d0
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-17 17:09:03 +0700

    menjalankan method dan menampilkan nya

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -31,3 +31,21 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nim = iNim;
 	nama = iNama;
 }
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << " Nama = " << nama << endl;
+}
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(30, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}

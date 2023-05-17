e20af4a36d9ab9415010adefbd530fc4da4fd429
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-17 18:18:24 +0700

    mengisi format data yakni cetak data dan juga isi data

--- destruktor/destruktor.cpp
+++ destruktor/destruktor.cpp
@@ -25,3 +25,16 @@ angka::~angka() { // Destructor
 	cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void  angka::cetakData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
+

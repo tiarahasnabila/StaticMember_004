5d7ac2575350530b1e615bf749ba6b5ea8cc345e
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-17 17:33:57 +0700

    mengisi obyek dengan id dan nama serta menampilkannya

--- mbrStatikfunc/mbrStatikfunc.cpp
+++ mbrStatikfunc/mbrStatikfunc.cpp
@@ -18,3 +18,15 @@ public:
 	mahasiswa(string pnama) :nama(pnama) { setID(); }
 };
 
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+

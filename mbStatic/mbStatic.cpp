8e19aa1af78a5cde19d6b989337902c4d7aeeff5
tiarahasnabila <tiarahasnabila@gmail.com>
2023-05-17 15:34:06 +0700

    method untuk menampilkan semua ID,Nama,Nim

--- mbStatic/mbStatic.cpp
+++ mbStatic/mbStatic.cpp
@@ -19,3 +19,9 @@ void mahasiswa::setID() {
 	id = ++nim;
 }
 
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << " NIM = " << nim << endl;
+	cout << endl;
+}
